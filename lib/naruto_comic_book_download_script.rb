460d10b09296ee1b8402bed5e68a3d6014bf17fd
shaobin0604 <shaobin0604@163.com>
2009-10-30 13:37:08 +0800

    add naruto download script

--- lib/naruto_comic_book_download_script.rb
+++ lib/naruto_comic_book_download_script.rb
@@ -1,21 +1,21 @@
 #--------------------------------------------------
-# Name:           fullmetal_alchemist_comic_book_download_script.rb
-# Description:    This Ruby script is used to batch download Fullmetal Alchemis
-#                 comic books which are hosted on http://www.jumpcn.com.
+# Name:           naruto_comic_book_download_script.rb
+# Description:    This Ruby script is used to batch download Naruto comic books
+#                 which are hosted on http://www.jumpcn.com.
 #
 #                 Make ensure wget program can be found via PATH environment
 #                 variable. The Windows version of wget program can be found
 #                 at http://users.ugent.be/~bpuype/wget/.
 #
 #                 Usage:
-#                 ruby fullmetal_alchemist_comic_book_download_script.rb chapter
+#                 ruby naruto_comic_book_download_script.rb chapter
 #
-#                 Note: 
+#                 Note:
 #                 the chapter argument must be an Integer. Up to now, the valid
-#                 chapter should be in eval((1..18).to_a + (74..100).to_a)
+#                 chapter should be in eval((1..40).to_a + (370..469).to_a)
 #
 # Author:         Bin Shao <shaobin0604@qq.com> http://aquarium.yo2.cn
-# Date:           Oct. 19, 2009
+# Date:           Oct. 30, 2009
 # License:        Apache License, Version 2.0
 #--------------------------------------------------
 require 'logger'
@@ -24,7 +24,7 @@ $log = Logger.new(STDOUT)
 $log.level = Logger::INFO
 
 # valid chapters up to now
-VALID_CHAPTERS_UPTO_NOW = (1..18).to_a + (74..100).to_a
+VALID_CHAPTERS_UPTO_NOW = (1..40).to_a + (370..469).to_a
 
 # Download pictures in chapter
 #
@@ -34,8 +34,8 @@ def download_chapter_pics(chapter)
   unless chapter_valid?(chapter)
     $log.error "chapter not valid"
   end
-  command_template = 'wget --directory-prefix="Fullmetal Alchemist/%03d"' +
-    ' http://gc.jumpcn.com/1/g/gang-zhi-lian-jin-shu-shi/%d/%03d.jpg'
+  command_template = 'wget --directory-prefix="Naruto/%03d"' +
+    ' http://gc.jumpcn.com/1/h/huo-ying-ren-zhe/%d/%03d.jpg'
   i = 0
   loop do
     i += 1
@@ -49,10 +49,10 @@ def chapter_valid?(chapter)
 end
 
 if __FILE__ == $0
-  
-  #  chapters = (1..18).to_a + (74..100).to_a
+
+  #  chapters = (1..40).to_a + (370..469).to_a
   if ARGV.length != 1
-    puts "Usage: ruby fullmetal_alchemist_comic_book_download_script.rb chapter"
+    puts "Usage: ruby naruto_comic_book_download_script.rb chapter"
     puts "chapter must be an Integer"
   else
     download_chapter_pics(ARGV[0].to_i)
